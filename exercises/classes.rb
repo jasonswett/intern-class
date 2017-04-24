6578fcb27ed04496fca083fe4608a4118d3ab2f7
Jason Swett <jason@benfranklinlabs.com>
2017-04-24 16:59:11 -0400

    Add menu items.

--- exercises/classes.rb
+++ exercises/classes.rb
@@ -1,7 +1,29 @@
 class MenuItem
   attr_accessor :name, :price
 
-  def to_s
-    "#{name} $#{price}"
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 end
+
+class Menu
+  def add_item(name, price)
+    @menu_items ||= []
+    @menu_items << MenuItem.new(name, price)
+  end
+
+  def to_s
+    @menu_items.map do |menu_item|
+      "#{menu_item.name} #{menu_item.price}"
+    end.join("\n")
+  end
+end
+
+menu = Menu.new
+menu.add_item('Hamburger', 5.00)
+menu.add_item('Cheeseburger', 5.50)
+menu.add_item('Small Fry', 2.00)
+menu.add_item('Large Fry', 2.50)
+
+puts menu
